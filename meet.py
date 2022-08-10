fb4bae25bbe52b048828cbe276641eccf402f309
Meet-khunt <110958152+Meet-khunt@users.noreply.github.com>
2022-08-10 09:31:42 +0530

    Add files via upload

---
+++ meet.py
@@ -0,0 +1,27 @@
+a="hallo"
+b="meet"
+
+print(a,b)
+
+print(type(1))
+print(type(1.1))
+print(type(()))
+print(type({}))
+print(type("hi"))
+
+
+name= input("what is ypur name ?")
+
+print(a+" "+name)
+
+
+print(20+50*10)
+
+
+
+class BigCar:
+    pass
+
+Obj1=BigCar
+
+print(type(Obj1))
