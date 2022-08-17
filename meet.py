4e80b2496534aca96d44bce1e1f984e175cc131b
Meet-khunt <110958152+Meet-khunt@users.noreply.github.com>
2022-08-17 15:51:16 +0530

    Update meet.py

--- meet.py
+++ meet.py
@@ -16,15 +16,16 @@ print("---------------------\n")
 
 name= input("what is ypur name ?")
 
-if name== "milan": #🤣
+if name== "milan": #true
     
     print(a+" "+name + " you are not abel  to see")
     exit()
-else:
+else: #false
   
  print(a+" "+name + " you are abel to see")
  
  
+#exit()
 
 print("---------------------\n")
 
@@ -47,5 +48,51 @@ print(type(str(1)))  #int in to str
 
 print("---------------------\n")
 
+print('''
+     MMM
+    |o o|
+      -
+     vvv
+''') #mod.string
 
+print("---------------------\n")
+
+weather="it\'s kaind\'of cold "  # \t for tabs
+
+print(weather)
+
+print("---------------------\n")
+
+#formatted string
+
+age=input("what is your age ?\n  ")
+
+print(f"hallo!! {name}. you are {age} year old")
+
+
+print("---------------------\n")
+
+#list
+
+list="dog pen kali leptop python"
+    # 0123456789......
+
+print(list[0:11]) #start froe 012345....
+
+
+print("---------------------\n")
+
+
+list2="dog pen kali leptop python"
+    # 0123456789......
+
+print(list2[::-1]) #revears the strings
+
+
+
+# [start:stop:jump]
+
+
+
+print("---------------------\n")
 
